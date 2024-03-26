f7a099eca64f3ffa04c7c7f32faebba0acaddc51
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-03-26 06:45:24 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+var name = "Ralph Mutonyi";
+
+console.log(name);
+
+let message = "i am a student at plp";
+
+console.log(message);
+
+const float = 3.1452;
+
+console.log(float);
+// variables in js are case sensitive
+
+// camelcase ---> myName
+
+// snake_case --> my_name , last_name
+
+
+
+
