ef69d56bd0ce21f71b4bb76390603dee01dd4512
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-11 10:06:44 +0300

    reached lesson 5 in js.info

---
+++ Javascript_fundamentals/alert.js
@@ -0,0 +1,18 @@
+let admin, name;
+
+name = "John";
+
+admin = name;
+
+// alert(admin);
+
+
+let ourPlanet = "Earth";
+
+let UserName = "John";
+
+const BIRTHDAY=  "24.06.2002";
+
+const age = somecode(BIRTHDAY);
+
+console.log(age);
