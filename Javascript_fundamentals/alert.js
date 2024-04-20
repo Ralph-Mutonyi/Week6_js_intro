6b81e6c40c535abaf230295cd29c57b65bfe2a67
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-04-20 18:42:51 +0300

    reviewed arrays

--- Javascript_fundamentals/alert.js
+++ Javascript_fundamentals/alert.js
@@ -1,9 +1,32 @@
-function ask (question,yes,no) {
-    (confirm(question)) ? yes() : no()
-}
+// Arrays Methods
 
-ask(
-    "Do you agree?",
-    () => alert("You agreed"),
-    () => alert("You canceled the execution")
-)
+// Push
+
+let myArray = ["Monday", "Tuesday","Wednesday"];
+
+console.log(myArray);
+
+myArray.push("Thursday");
+
+console.log(myArray);
+
+
+// pop methods
+
+myArray.pop("Thursday");
+console.log(myArray);
+
+
+myArray.pop(2);
+console.log(myArray);
+
+// unshift
+
+myArray.unshift("Sunday");
+console.log(myArray);
+
+// shift
+
+myArray.shift(0);
+
+console.log(myArray);
